c29100d804067ac9031a17a20c574730327167bb
Rafal <rafal.z.malucha@gmail.com>
2025-06-19 18:03:27 +0200

    microservice scaffolding

---
+++ src/main/java/com/bugtracker/projectservice/ProjectController.java
@@ -0,0 +1,13 @@
+package com.bugtracker.projectservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProjectController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "This is a test - project-service";
+    }
+}
